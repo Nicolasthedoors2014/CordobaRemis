583da23f26924d85118b8e526050c4960f631b8c
nicolasthedoors2014 <nicolasthedoors2014@gmail.com>
2017-07-01 21:11:58 -0300

    locaciones cargadas a la base de datos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create([{name: "Alta Córdoba", x_coordinate: 1, y_coordinate: 1},
+ {name: "Patio Olmos", x_coordinate: -0.3, y_coordinate: -0.2},
+ {name: "Plaza San Martín", x_coordinate: 0, y_coordinate: 0},
+ {name: "Parque Sarmiento", x_coordinate: 1, y_coordinate: -1},
+ {name: "Cerro de las Rosas", x_coordinate: -4, y_coordinate: 4},
+ {name: "Aeropuerto", x_coordinate: 0, y_coordinate: 7},
+ {name: "Terminal de Ómnibus", x_coordinate: 0, y_coordinate: 1}])
+
+p "Created #{Location.count} locations"
